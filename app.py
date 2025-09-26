d8091386ffb2ea6dc09505334cf592718046c20c
KrzWit <164687001+KrzWit@users.noreply.github.com>
2025-09-26 19:41:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,5 +108,7 @@ def delete_note(note_id):
         _write_all(new_notes)
     return "", 204
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
